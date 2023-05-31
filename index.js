903faaeaf2b7faac5200d3b4835f19e1188d6782
itisri <itisrichaudhuryiti@gmail.com>
2023-05-31 17:17:36 +0530

    Calender created

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 
 const monthE1=document.querySelector(".date h1");
 const fullDateE1=document.querySelector(".date p");
+const daysE1= document.querySelector(".days");
+
 const monthInx= new Date().getMonth();
-const lastDay= new Date(new Date().getFullYear, monthInx + 1, 0);
-
-
+const lastDay= new Date(new Date().getFullYear, monthInx + 1, 0).getDate();
 
 
 
@@ -25,3 +25,19 @@ const months=[
 ];
 monthE1.innerText = months[monthInx];
 fullDateE1.innerText = new Date().toDateString();
+
+
+let days= "";
+
+for( let i= firstDay; i>0 ;i--){
+    days += `<div class="empty"></div>`;
+}
+for(let i=1; i<= lastDay;i++){
+    if(i === new Date().getDate()){
+        days += `<div class="today">${i}</div>`;
+    } else {
+        days += `<div> ${i}</div>`;
+    }
+}
+                                                                                                    
+daysE1.innerHTML = days;
